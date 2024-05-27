76006298f85266471c5c47ff8ef0572b87235b17
1NT21CS <157352436+1NT21CS@users.noreply.github.com>
2024-05-27 05:30:02 +0530

    Update HillCipher.c

--- HillCipher.c
+++ HillCipher.c
@@ -1,41 +1,51 @@
+
 #include <stdio.h>
 #include <string.h>
-int main()
-{
-unsigned int a[3][3] = {{6,24,1}, {13,16,10}, {20,17,15}};
-unsigned int b[3][3] = {{8,5,10}, {21,8,21}, {21,12,8}};
-int i,j;
-unsigned int c[20],d[20];
-char msg[20];
-int determinant = 0,t=0;
-;
-printf("Enter the plaintext: \n");
-scanf("%s", msg);
-for(i=0;i<3;i++){
-c[i] = msg[i] - 65;
-printf("%d", c[i]);
-}
-for(i=0;i<3;i++){
-t=0;
-for(j=0;j<3;j++){
-t= t + (a[i][j]*c[j]);
-}
-d[i] = t%26;
-}
-printf("\nencrypted cipher text: ");
-for(i=0;i<3;i++)
-printf("%c",d[i] + 65);
-for(i=0;i<3;i++){
-t=0;
-for(j=0;j<3;j++){
-t = t + (b[i][j]*d[j]);
-}
-c[i] = t%26;
-}
-printf("\ndecrypted cipher text: ");
-for(i=0;i<3;i++)
-{
-printf("%c", c[i]+65);
-}
-return 0;
+void encrypt(int a[3][3], unsigned int *c, unsigned int *d, int len) {
+ int t, i, j, k = 0;
+ for (k = 0; k < len; k += 3) {
+ for (i = 0; i < 3; i++) {
+ t = 0;
+ for (j = 0; j < 3; j++) {
+ t += a[i][j] * c[k + j];
+ }
+ d[k + i] = t % 26;
+ }}}
+void decrypt(int b[3][3], unsigned int *d, unsigned int *c, int len) {
+ int t, i, j, k = 0;
+ for (k = 0; k < len; k += 3) {
+ for (i = 0; i < 3; i++) {
+ t = 0;
+ for (j = 0; j < 3; j++) {
+ t += b[i][j] * d[k + j];
+ }
+ c[k + i] = t % 26;
+ }}}
+int main() {
+ unsigned int a[3][3] = {{6, 24, 1}, {13, 16, 10}, {20, 17, 15}};
+ unsigned int b[3][3] = {{8, 5, 10}, {21, 8, 21}, {21, 12, 8}};
+ unsigned int c[100], d[100];
+ char msg[100];
+ int original_len, padded_len, i;
+ printf("Enter the plaintext: ");
+ scanf("%s", msg);
+ original_len = strlen(msg);
+ padded_len = original_len;
+ while (padded_len % 3 != 0) {
+ msg[padded_len] = 'X';
+ padded_len++;
+ }
+ msg[padded_len] = '\0';
+ for (i = 0; i < padded_len; i++)
+ c[i] = msg[i] - 65;
+ encrypt(a, c, d, padded_len);
+ printf("\nEncrypted cipher text: ");
+ for (i = 0; i < padded_len; i++)
+ printf("%c", d[i] + 65);
+ decrypt(b, d, c, padded_len);
+ printf("\nDecrypted cipher text: ");
+ for (i = 0; i < original_len; i++)
+ printf("%c", c[i] + 65);
+ printf("\n");
+ return 0;
 }
